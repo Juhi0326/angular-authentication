bab8242586fdf1a1bc681eea7dc99faac4e6c8ab
juhi <juhi0326@gmail.com>
2019-04-25 08:32:46 +0200

    binding children routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
     component: ProjectComponent,
 
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'forgot-password', component: ForgotPasswordComponent },
